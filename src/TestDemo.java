c117e097bf8702c869d48ad2bfeb154e8e43dd14
Wan9N1n9 <1289050680@qq.com>
2023-04-06 20:32:39 +0800

    first JavaSE

---
+++ src/TestDemo.java
@@ -0,0 +1,30 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+/**
+ * Program: Created with IntelliJ IDEA
+ * Description:
+ * psvm main()函数
+ * sout :system.out.println()
+ * Author: Wan9N1n9
+ * Time:2023-03-01 11:40
+ */
+public class TestDemo {
+    /*
+    块注释
+     */
+
+    /**
+     * 文档注释
+     * @param args
+     */
+    public static void main(String[] args) {
+        int c = Integer.MAX_VALUE + 1 ;
+        System.out.println(c);
+        int d = Integer.MIN_VALUE;
+        System.out.println(d);
+        int a = 10; //二进制存储 行注释
+        int b = 20;
+        System.out.println(a);
+        System.out.println("HelloWorld");
+    }
+}
